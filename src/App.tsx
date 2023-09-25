269eb56c680cc6b66726bc3d3dc91984486e457f
daria-kawecka <daria.kawecka28@gmail.com>
2023-09-25 20:04:52 +0200

    Create app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
-export const App = () => <main>app</main>;
+import { AppLayout } from 'containers/AppLayout/AppLayout';
+import { Routes } from 'routes/Routes';
+import { Theme } from 'theme';
+
+export const App = () => (
+  <Theme>
+    <AppLayout>
+      <Routes />
+    </AppLayout>
+  </Theme>
+);
