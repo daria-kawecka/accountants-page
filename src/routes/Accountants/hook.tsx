a49a7ed91581eaa4c328f78c50366dc73d48376c
daria-kawecka <daria.kawecka28@gmail.com>
2023-09-25 20:39:49 +0200

    Remove ref

--- src/routes/Accountants/hook.tsx
+++ src/routes/Accountants/hook.tsx
@@ -5,7 +5,6 @@ import { INITIAL_ACCOUNTANTS_RESPONSE } from './constants';
 export const useAccountantsData = (page: number) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const isInitialRender = useRef(true);
 
   const [accountantsData, setAccountantsData] =
     useState<AccountantResponseType>(INITIAL_ACCOUNTANTS_RESPONSE);
@@ -30,12 +29,7 @@ export const useAccountantsData = (page: number) => {
   }, [page]);
 
   useEffect(() => {
-    // Skip fetch on the initial render cause in R18 it's rerender twice
-    if (isInitialRender.current) {
-      isInitialRender.current = false;
-    } else {
-      fetchPeopleInformation();
-    }
+    fetchPeopleInformation();
   }, [fetchPeopleInformation]);
 
   return { isLoading, isError, accountantsData };
